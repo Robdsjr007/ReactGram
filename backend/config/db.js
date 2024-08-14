0a5e21509ddb3ffeb26d34f98b08d12a5d73235c
Robdsjr007 <robson.santana15@etec.sp.gov.br.com>
2024-08-13 22:45:19 -0300

    raw auth with token

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASS;
 const conn = async () => {
     try {
         const dbConn = await mongoose.connect(
-            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.qsnos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`        
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.qsnos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
         );
         
         console.log("Conectou ao banco!");
