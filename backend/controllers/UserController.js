59ce67ed0e3ceeeb99a69abb0d2b41b3e1b20b5b
Robdsjr007 <robson.santana15@etec.sp.gov.br>
2024-09-04 20:32:43 -0300

    feat get user by id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -27,12 +27,12 @@ const register = async (req, res) => {
     }
 
     //Function generate password hash
-    const generateHash = async(password) => {
+    const generateHash = async (password) => {
         const salt = await bcrypt.genSalt()
         const passwordHash = await bcrypt.hash(password, salt)
         return passwordHash
     }
-        
+
 
 
     generateHash(password);
@@ -45,8 +45,8 @@ const register = async (req, res) => {
     })
 
     // if user was created succesfuly
-    if(!newUser) {
-        res.status(422).json({errors: ["Houve um erro no registro, tente mais tarde."]})
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro no registro, tente mais tarde."] })
         return
     }
 
@@ -58,19 +58,19 @@ const register = async (req, res) => {
 
 // Sign user in
 const login = async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
     // Check if user exists
-    if(!user) {
-        res.status(404).json({errors: ["Usuário não encontrado."]})
+    if (!user) {
+        res.status(404).json({ errors: ["Usuário não encontrado."] })
         return
     }
 
     // Check if password matches
-    if(!(await bcrypt.compare(password, user.password))) {
-        res.status(422).json({errors: ["Senha inválida."]})
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({ errors: ["Senha inválida."] })
         return
     }
 
@@ -82,18 +82,18 @@ const login = async (req, res) => {
 };
 
 // Get current logged in user
-const getCurrentUser = async(req, res) => {
+const getCurrentUser = async (req, res) => {
     const user = req.user;
     res.status(200).json(user);
 }
 
 //update an user
-const update = async(req, res) => {
-    const {name, password, bio} = req.body;
+const update = async (req, res) => {
+    const { name, password, bio } = req.body;
 
     let profileImage = null;
 
-    if(req.file) {
+    if (req.file) {
         profileImage = req.file.filename;
     }
 
@@ -101,20 +101,20 @@ const update = async(req, res) => {
 
     const user = await User.findById(reqUser._id).select("-password");
 
-    if(name) {
+    if (name) {
         user.name = name;
     }
 
-    if(password) {
+    if (password) {
         const passwordHash = await generateHash(password);
         user.password = passwordHash;
     }
 
-    if(profileImage) {
+    if (profileImage) {
         user.profileImage = profileImage;
     }
 
-    if(bio) {
+    if (bio) {
         user.bio = bio;
     }
 
@@ -123,9 +123,31 @@ const update = async(req, res) => {
     res.status(200).json(user);
 };
 
+// Get user by id
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id).select("-password");
+
+        // Does not pass to the second if the id is the same but not valid.
+        if (!user) {
+            res.status(404).json({ errors: ["Usuário não encontrado."] });
+            return
+        }
+    } catch (error) {
+        // ID invalid
+        res.status(404).json({ errors: ["Usuário não encontrado."] });
+        return
+    }
+
+    res.status(200).json(user);
+}
+
 module.exports = {
     register,
     login,
     getCurrentUser,
     update,
+    getUserById
 };
