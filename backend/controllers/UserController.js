a12a84563b0ac65f417d6dd95629e94db04dceb1
Robdsjr007 <robson.santana15@etec.sp.gov.br>
2024-09-04 20:13:33 -0300

    feat update image and fields

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const mongoose = require("mongoose");
+
 const jwtSecret = process.env.JWT_SECRET;
 
 // Generate user token
@@ -24,9 +26,16 @@ const register = async (req, res) => {
         return
     }
 
-    // Generate password hash
-    const salt = await bcrypt.genSalt()
-    const passwordHash = await bcrypt.hash(password, salt)
+    //Function generate password hash
+    const generateHash = async(password) => {
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+        return passwordHash
+    }
+        
+
+
+    generateHash(password);
 
     // Create user
     const newUser = await User.create({
@@ -78,8 +87,45 @@ const getCurrentUser = async(req, res) => {
     res.status(200).json(user);
 }
 
+//update an user
+const update = async(req, res) => {
+    const {name, password, bio} = req.body;
+
+    let profileImage = null;
+
+    if(req.file) {
+        profileImage = req.file.filename;
+    }
+
+    const reqUser = req.user;
+
+    const user = await User.findById(reqUser._id).select("-password");
+
+    if(name) {
+        user.name = name;
+    }
+
+    if(password) {
+        const passwordHash = await generateHash(password);
+        user.password = passwordHash;
+    }
+
+    if(profileImage) {
+        user.profileImage = profileImage;
+    }
+
+    if(bio) {
+        user.bio = bio;
+    }
+
+    await user.save();
+
+    res.status(200).json(user);
+};
+
 module.exports = {
     register,
     login,
     getCurrentUser,
+    update,
 };
---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const multer = require("multer");
+const path = require("path");
+
+// Destination to store image
+const imageStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = ""
+
+        if (req.baseUrl.includes("users")) {
+            folder = "users"
+        } else if(req.baseUrl.includes("photos")) {
+            folder = "photos" 
+        }
+
+        const uploadPath = path.join(__dirname, '..', 'uploads', folder);
+
+        // Cria o diretório se não existir
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(png|jpg)$/)) {
+            // upload only png and jpg formats
+            return cb(new Error("Por favor, envie apenas png ou jpg!"))
+        }
+        cb(undefined, true)
+    }
+})
+
+module.exports = { imageUpload };
