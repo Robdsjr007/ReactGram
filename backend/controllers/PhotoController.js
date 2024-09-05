8d2b7073e3de85184deac6f3e2c28e9b1e2110df
Robdsjr007 <robson.santana15@etec.sp.gov.br>
2024-09-04 21:27:23 -0300

    feat delete a photo by id

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -8,28 +8,63 @@ const insertPhoto = async (req, res) => {
     const { title } = req.body;
     const image = req.file.filename;
 
-    const reqUser = req.user
+    const reqUser = req.user;
 
     const user = await User.findById(reqUser._id);
 
     // Create a photo
 
     const newPhoto = await Photo.create({
-        image, 
-        title, 
+        image,
+        title,
         userId: user._id,
         userName: user.name,
     });
 
     // If photo was created successfully, return data
-    if(!newPhoto) {
-        res.status(422).json({errors: ["Tente novamente mais tarde!"]});
+    if (!newPhoto) {
+        res.status(422).json({ errors: ["Tente novamente mais tarde!"] });
+        return;
     }
-    
 
-    res.status(201).json(newPhoto)
+
+    res.status(201).json(newPhoto);
+};
+
+// Remove a photo from DB
+
+const deletePhoto = async(req, res) => {
+    const { id } = req.params;
+    const reqUser = req.user;
+
+    try {
+        const photo = await Photo.findById((id));
+
+
+        // Check if photo exists
+        if (!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada!"] });
+            return;
+        }
+
+        // Check if photo belongs to user
+        if (!photo.userId.equals(reqUser._id)) {
+            res.status(422).json({ errors: ["Ocorreu um erro, tente novamente mais tarde."], });
+        }
+
+        await Photo.findByIdAndDelete(photo._id);
+
+        res.status(200).json({ id: photo._id, message: "Foto excluída com suceso." });
+    } catch (error) {
+        // ID invalid
+        console.error("Erro ao inserir foto:", error);
+        res.status(404).json({errors: ["Foto não encontrada."], });
+        return;
+    };
+
 };
 
 module.exports = {
     insertPhoto,
+    deletePhoto
 };
