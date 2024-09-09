82d525551285e559386fcc7d80fa13ac42bf0c93
Robdsjr007 <robson.santana15@etec.sp.gov.br>
2024-09-09 19:00:08 -0300

    Layout changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+// Hooks
+import { useAuth } from './hooks/useAuth';
+
 // estilo
 import './styles/app.sass';
 
@@ -11,15 +14,22 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const App = () => {
+  const {auth, loading} = useAuth();
+
+// Carregando componentes
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className='App'>
       <BrowserRouter>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
+            <Route path='/' element={auth ? <Home /> : <Navigate to="/login"/>} />
+            <Route path='/login' element={!auth ? <Login /> : <Navigate to="/"/> } />
+            <Route path='/register' element={!auth ? <Register /> : <Navigate to="/"/>} />
           </Routes>
         </div>
         <Footer />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import { NavLink, Link } from "react-router-dom"
 import { BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill } from "react-icons/bs";
 
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 // estilo
 import '../styles/components/navbar.sass'
 
 const Navbar = () => {
+    const { auth } = useAuth();
+    const { user } = useSelector((state) => state.auth);
+
+
     return (
 
         <nav id="nav">
@@ -15,21 +25,44 @@ const Navbar = () => {
                 <input type="text" placeholder="Pesquisar" />
             </form>
             <ul id="nav-links">
-                <li>
-                    <NavLink to="/">
-                        <BsHouseDoorFill />
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">
-                        Entrar
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/register'>
-                        Cadastrar
-                    </NavLink>
-                </li>
+                {auth ? (
+                    <>
+                        <li>
+                            <NavLink to="/">
+                                <BsHouseDoorFill />
+                            </NavLink>
+                        </li>
+                        {user && (
+                            <li>
+                                <NavLink to={`/usets/${user._id}`}>
+                                    <BsFillCameraFill />
+                                </NavLink>
+                            </li>
+                        )}
+                        <li>
+                            <NavLink to="/profile">
+                                <BsFillPersonFill />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <span>Sair</span>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <NavLink to='/login'>
+                                Entrar
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/register'>
+                                Cadastrar
+                            </NavLink>
+                        </li>
+                    </>
+                )}
+
             </ul>
 
         </nav>
